468ef1d2b916d713c7ede6feb387386e037ccd0a
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-03-06 17:34:06 +0700

    Tipe Data String

---
+++ string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Andhika")
+	fmt.Println("Andhika Juliawan")
+
+	fmt.Println(len("Andhika"))
+	fmt.Println("Andhika Juliawan"[0])
+	fmt.Println("Andhika Juliawan"[1])
+}
