1df2af0c61cbe42f0bebe6aecd95b928ca16981f
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-03-06 17:50:45 +0700

    Variable

---
+++ variable.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "Andhika Juli"
+	fmt.Println(name)
+
+	name = "Andhika Juliawan"
+	fmt.Println(name)
+
+	var (
+		firstName  = "Andhika"
+		middleName = "Juli"
+		lastName   = "awan"
+	)
+	fmt.Println(firstName)
+	fmt.Println(middleName)
+	fmt.Println(lastName)
+}
