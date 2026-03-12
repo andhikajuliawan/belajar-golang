63b2347ac6cfe1b281622fc0f4c346158fbd7c91
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-03-12 13:43:54 +0700

    Type Assertions

---
+++ type_assertions.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func random() interface{} {
+	return "OK"
+}
+
+func main() {
+	var result any = random()
+	// var resultString string = result.(string)
+	// fmt.Println(resultString)
+	// var resultInt int = result.(int) // Panic
+	// fmt.Println(resultInt)
+
+	switch value := result.(type) {
+	case string:
+		fmt.Println("String", value)
+	case int:
+		fmt.Println("Integer", value)
+	default:
+		fmt.Println("Unknown", value)
+	}
+
+}
